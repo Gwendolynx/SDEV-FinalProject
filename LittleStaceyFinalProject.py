da1b1c84d241ac7bfe9d0c8109f75384236e187a
Gwendolynx <137134978+Gwendolynx@users.noreply.github.com>
2024-10-07 15:03:20 -0500

    Add files via upload

--- LittleStaceyFinalProject.py
+++ LittleStaceyFinalProject.py
@@ -20,7 +20,7 @@ def main():
     print("Thank you. Here is information regarding " + drug_search + ":")
     print()
 
-    # import csv with drug information
+    # import csv with sample drug information
     import csv
 
     # display and format the spacing of the search results header
@@ -51,40 +51,52 @@ def main():
                 print("{: <30} {: <25} {: <15}".format(*search2))
                 print('------------------------------------------------------------------------')   
 
+
+
+
+
+
+
 # window created for second window      
 class Window(tk.Toplevel):
     def search_results(self):
         global t1
         t1 = Label(self, text="Thank you. Here is information regarding " + drug_search + ":" + "\n" + "{: <30} {: <25} {: <15}".format(*header) + "\n").grid(row=3,column=0,columnspan=4)
+# tests if 'search1' was created in main() function and displays the output
         try:
             search1
             global t2
             t2 = Label(self, text="{: <30} {: <25} {: <15}".format(*search1)).grid(row=4,column=0,columnspan=4)
 
-            
+# if 'search1' was not created in main(), this means 'search2' was created instead
+# and displays that output instead
         except:
             global t3
             t3 = Label(self, text="{: <30} {: <25} {: <15}".format(*search2)).grid(row=4,column=0,columnspan=4)
             
-
+# format window
     def __init__(self, parent):
         super().__init__(parent)
-# format window
         self.configure(background="pink")      
         self.geometry('400x220')
+# adds window title
         self.title('Drug Input')
-
+# related image inserted per project requirements
         global image2
         image2 = tk.PhotoImage(file="pill2.gif")
         tk.Label(self, image=image2).grid(row=0,column=0,columnspan=3,pady=5)
+# brief instruction provided to user
         my_text2 = Label(self, text="Enter a drug name below", font="Times 12 bold").grid(row=1,column=0,columnspan=2,padx=100)
+# user input area created with label to the side for user direction
         drug_text = Label(self, text="Drug: ").grid(row=2,column=0)
 
         global drug
         drug = Entry(self)
-
         drug.grid(row=2,column=0,columnspan=2,padx=50)
+# buttons created to either perform the drug search or restart back to main window
         but4 = Button(self, text="Restart", command=self.destroy).grid(row=2,column=1)
+# this button functions by calling the main() function to perform the drug search
+# then the search_results function is called to actually display the results
         but3 = Button(self, text="Search", command=lambda:[main(),Window.search_results(self)]).grid(row=3,column=0,columnspan=2)
     
 
@@ -94,28 +106,33 @@ class Window(tk.Toplevel):
 
 
 
-# window created for the main (parent) window
+# window created for the main (opening) window
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-# format window to look nicer
+# format window to look nicer and fit image
         self.configure(background="pink")
         self.geometry('430x300')
-# give window a title and some basic text to welcome the user
+        self.title('Top 100 Drugs Cheatsheet')
+
+# related image inserted per project requirements
         global image
         image = tk.PhotoImage(file="pill1.gif")
-        self.title('Top 100 Drugs Cheatsheet')
         tk.Label(self, image=image).grid(row=0,column=0)
+        
+# give window a title and some basic text to welcome the user and provide instruction
         my_text1 = Label(self, text="Welcome to my program!", font="Times 16 bold").grid(row=1,column=0,padx=60,pady=15)
         my_text2 = Label(self, text="We're here for all your drug information needs.").grid(row=2,column=0,padx=60,pady=15)
         my_instruction = Label(self, text="To begin, please click 'Open Drug Input' below." + "\n" + "To exit the program, please click 'close'", font="Arial 10 bold").grid(row=2,column=0,padx=60,pady=15)
-# button to open the drug search tool
+# buttons to open the drug search tool and to close the program
         but1 = Button(self, text='Open Drug Input', command=self.open_window).grid(row=3,column=0, pady = 15)
         close_but = Button(self, text="Close", command=self.destroy).grid(row=4, column=0)
-        
+# function to open the search window. This function is being called on the 'Open Drug Input' button above
     def open_window(self):
         window = Window(self)
         window.grab_set()
+
+
 
 # run the program
 if __name__ == "__main__":
